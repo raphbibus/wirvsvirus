a47253c5b19eacbd929c796d0c260bcfc40ae85b
rfha <rfha1234@gmail.com>
2020-03-21 18:25:39 +0100

    Add the home enter and home leave requests

--- utils/api.js
+++ utils/api.js
@@ -25,13 +25,46 @@ export default class Api {
     const response = await fetch(BASE_URL + "users/" + userName, {
       headers: HEADERS
     });
+
+    if (response.status > 299) throw new Error(await response.text());
+
     return await response.json();
   }
 
   static async getUserStats(userName) {
-    const response = await fetch(BASE_URL + "users/" + username + "/stats", {
+    const response = await fetch(BASE_URL + "users/" + userName + "/stats", {
       headers: HEADERS
-    })
+    });
+
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
+  static async createEnteredHomeEvent(userName, timestamp) {
+    const response = await fetch(BASE_URL + "users/" + userName + "/home-enter", {
+      method: "POST",
+      headers: HEADERS
+      body: JSON.stringify({
+        timestamp: timestamp
+      })
+    });
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
+  static async createEnteredHomeEvent(userName, timestamp, token) {
+    const response = await fetch(BASE_URL + "users/" + userName + "/home-leave", {
+      method: "POST",
+      headers: HEADERS
+      body: JSON.stringify({
+        timestamp: timestamp,
+        token: token
+      })
+    });
+    if (response.status > 299) throw new Error(await response.text());
+
     return await response.json();
   }
 }
