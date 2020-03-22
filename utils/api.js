4a355678df7df7b244331dfbbd63b3df576123d8
rfha <rfha1234@gmail.com>
2020-03-22 13:42:48 +0100

    Sort api requests the same as in the api documentation

--- utils/api.js
+++ utils/api.js
@@ -6,6 +6,53 @@ const HEADERS = {
 };
 
 export default class Api {
+  static async getUserStats(userName) {
+    const response = await fetch(BASE_URL + "users/" + userName + "/stats", {
+      headers: HEADERS
+    });
+
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
+  static async createEnteredHomeEvent(userName, timestamp) {
+    const response = await fetch(BASE_URL + "users/" + userName + "/home-enter", {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify({
+        timestamp: timestamp
+      })
+    });
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
+  static async createLeftHomeEvent(userName, timestamp, token) {
+    const response = await fetch(BASE_URL + "users/" + userName + "/home-leave", {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify({
+        timestamp: timestamp,
+        token: token
+      })
+    });
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
+  static async getUser(userName) {
+    const response = await fetch(BASE_URL + "users/" + userName, {
+      headers: HEADERS
+    });
+
+    if (response.status > 299) throw new Error(await response.text());
+
+    return await response.json();
+  }
+
   static async createUser(userName, displayName) {
     const response = await fetch(BASE_URL + "users", {
       method: "POST",
@@ -65,53 +112,6 @@ export default class Api {
       })
     });
 
-    if (response.status > 299) throw new Error(await response.text());
-
-    return await response.json();
-  }
-
-  static async getUser(userName) {
-    const response = await fetch(BASE_URL + "users/" + userName, {
-      headers: HEADERS
-    });
-
-    if (response.status > 299) throw new Error(await response.text());
-
-    return await response.json();
-  }
-
-  static async getUserStats(userName) {
-    const response = await fetch(BASE_URL + "users/" + userName + "/stats", {
-      headers: HEADERS
-    });
-
-    if (response.status > 299) throw new Error(await response.text());
-
-    return await response.json();
-  }
-
-  static async createEnteredHomeEvent(userName, timestamp) {
-    const response = await fetch(BASE_URL + "users/" + userName + "/home-enter", {
-      method: "POST",
-      headers: HEADERS,
-      body: JSON.stringify({
-        timestamp: timestamp
-      })
-    });
-    if (response.status > 299) throw new Error(await response.text());
-
-    return await response.json();
-  }
-
-  static async createEnteredHomeEvent(userName, timestamp, token) {
-    const response = await fetch(BASE_URL + "users/" + userName + "/home-leave", {
-      method: "POST",
-      headers: HEADERS,
-      body: JSON.stringify({
-        timestamp: timestamp,
-        token: token
-      })
-    });
     if (response.status > 299) throw new Error(await response.text());
 
     return await response.json();
